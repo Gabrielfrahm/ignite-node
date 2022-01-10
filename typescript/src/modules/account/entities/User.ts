25a61c31fba4b080ae08b8861362989b9567979b
Gabriel Marques <gabriel_frahm@hotmail.com>
2022-01-10 20:45:19 -0300

    ✨ add migration and model user for module account

---
+++ typescript/src/modules/account/entities/User.ts
@@ -0,0 +1,46 @@
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
